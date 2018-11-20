4ae0df635c185df0877701f89047ee335a15ac4c
OsJanczak <os.janczak@gmail.com>
2018-11-20 10:12:31 +0100

    changed tests description

--- zjprog2018/tests/test/gilded_rose.spec.js
+++ zjprog2018/tests/test/gilded_rose.spec.js
@@ -6,7 +6,7 @@ describe('Gilded Rose', () => {
   const backstageItem = 'Backstage passes to a TAFKAL80ETC concert';
   const Sulfuras = 'Sulfuras, Hand of Ragnaros';
 
-  it('Should use null as zero value', () => {
+  it('Should use null as 0', () => {
     const gildedRose = new Shop([new Item('null', null, null)]);
 
     const item = gildedRose.updateQuality();
@@ -61,7 +61,7 @@ describe('Gilded Rose', () => {
   });
 
   describe('Normal Item', () => {
-    it('should decrease quality and sellIn by 1', () => {
+    it('Should decrease quality and sellIn by 1', () => {
       const gildedRose = new Shop([new Item('normal item', 12, 1)]);
 
       const item = gildedRose.updateQuality();
@@ -70,7 +70,7 @@ describe('Gilded Rose', () => {
       expect(item[0].sellIn).to.equal(11);
     });
 
-    it('should decrease quality by 2 if sellin is above 0', () => {
+    it('Should decrease quality by 2 if sellin is above 0', () => {
       const gildedRose = new Shop([new Item('normal item', -1, 50)]);
 
       const items = gildedRose.updateQuality();
@@ -79,7 +79,7 @@ describe('Gilded Rose', () => {
       expect(items[0].quality).to.equal(48);
     });
 
-    it('should not decrease quality below zero', () => {
+    it('Should not decrease quality below zero', () => {
       const gildedRose = new Shop([new Item('normal item', 6, 0)]);
 
       const item = gildedRose.updateQuality();
@@ -121,8 +121,8 @@ describe('Gilded Rose', () => {
     ];
 
     GIVEN_VALUES.forEach(({ name, given, multipler, end }) => {
-      it(`should increase quality of ${name} by ${multipler}
-      if day to sellin is beetwen ${given.sellIn - 1} and ${end}`, () => {
+      it(`Should increase quality of ${name} by ${multipler}
+      if sellin is between ${given.sellIn - 1} and ${end}`, () => {
         let items;
         let gildedRose;
 
@@ -140,7 +140,7 @@ describe('Gilded Rose', () => {
       });
     });
 
-    it('should set quality at 0 if sellIn is below 0', () => {
+    it('Should set quality at 0 if sellIn is below 0', () => {
       const gildedRose = new Shop([new Item(backstageItem, 0, 50)]);
 
       const item = gildedRose.updateQuality();
@@ -150,7 +150,7 @@ describe('Gilded Rose', () => {
     });
 
     describe('Sulfuras', () => {
-      it('should never decrease quality and sellIn', () => {
+      it('Should never decrease quality and sellIn', () => {
         const gildedRose = new Shop([new Item(Sulfuras, 3, 2)]);
 
         let items;
@@ -164,7 +164,7 @@ describe('Gilded Rose', () => {
     });
 
     describe('Aged Brie', () => {
-      it('should increase quality by 1 and decrease sellIn by 1', () => {
+      it('Should increase quality by 1 and decrease sellIn by 1', () => {
         const gildedRose = new Shop([new Item(agedBrieItem, 5, 10)]);
 
         const items = gildedRose.updateQuality();
@@ -173,7 +173,7 @@ describe('Gilded Rose', () => {
         expect(items[0].quality).to.equal(11);
       });
 
-      it('should doesnt increase quality higer than 50', () => {
+      it('Should doesnt increase quality higer than 50', () => {
         const gildedRose = new Shop([new Item(agedBrieItem, 5, 50)]);
 
         const items = gildedRose.updateQuality();
@@ -184,7 +184,7 @@ describe('Gilded Rose', () => {
     });
 
     describe('Conjured', () => {
-      it('should decrease quality of conjured items by 2', () => {
+      it('Should decrease quality of conjured items by 2', () => {
         const gildedRose = new Shop([new Item('conjured', 15, 23)]);
 
         const items = gildedRose.updateQuality();
